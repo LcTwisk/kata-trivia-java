c9cb46fd3282acd1bb9503abfc90a9bdc48b7c61
Lucas Twisk <lucas.twisk@gmail.com>
2022-06-19 15:37:42 +0200

    Add some clarifications to confusing method definitions in `IGame`

--- src/main/java/trivia/IGame.java
+++ src/main/java/trivia/IGame.java
@@ -6,8 +6,16 @@ public interface IGame {
 
 	void roll(int roll);
 
+	/**
+	 * Processes a correct answer.
+	 *
+	 * @return boolean true if the current player has not(!) won as a result of the answer. */
 	boolean processCorrectAnswer();
 
+	/**
+	 * Processes an incorrect answer.
+	 *
+	 * @return boolean true if the current player has not(!) won as a result of the answer. */
 	boolean processWrongAnswer();
 
 }
